0c8454a7f45704a97a3c7c58c28a7acdcf867d50
László Őri <laszlo.oeri@gmail.com>
2019-06-26 22:56:44 +0200

    chore(travis): setup test runner ERRAND-2

---
+++ ErrandTestRunner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ErrandTestRunner
+//
+//  Created by LasOri on 2019. 06. 26..
+//  Copyright © 2019. LasOri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
